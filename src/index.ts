1d696a2bfd3b3685acc8a1681dc248208f76d6ed
Alejandro Cerezo <alce65@hotmail.es>
2023-02-28 13:01:40 +0100

    ✨ Install mongoose and connect

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import http from 'http';
 import { app } from './app.js';
+import { dbConnect } from './db/db.connetc.js';
 
 const PORT = process.env.PORT || 4500;
 
 const server = http.createServer(app);
 
-server.listen(PORT);
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    console.log('DB:', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => server.emit('error', error));
 
-server.on('error', () => {
-  // SIn hacer
+server.on('error', (error) => {
+  console.error('Server error:', error.message);
 });
 
 server.on('listening', () => {
